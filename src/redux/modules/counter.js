f2492737ee4d05aadbabddedea0971638ad82e8a
stopbrother <stopbro0323@naver.com>
2024-08-20 11:12:57 +0900

    add : 모듈 만들기

---
+++ src/redux/modules/counter.js
@@ -0,0 +1,13 @@
+const initialState = {
+  number: 0,
+};
+
+const counter = (state = initialState, action) => {
+  switch (action.type) {
+    case value:
+    default:
+      return state;
+  }
+};
+
+export default counter;
